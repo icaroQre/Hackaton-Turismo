a86f625a747878d85ca18174f397d84ba98a3fef
icaroQre <icaro.queiroz.reccanello2@gmail.com>
2022-09-15 22:05:41 -0300

    testando responsibilidade

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -7,10 +7,10 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
         text-decoration: 0;
         background-color: white;
-        overflow-x: hidden;
         max-width: 100vw;
         font-family: 'Montserrat', sans-serif;
         font-weight: 500;
+        overflow-x: hidden;
     }
 `;
 
