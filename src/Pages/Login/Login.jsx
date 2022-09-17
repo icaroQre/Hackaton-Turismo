a1aa39977a6462a8a8a541f74eb5bc6eb096b1c1
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-09-17 03:09:14 -0300

    logo na tela de login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import GlobalStyles from '../../styles/GlobalStyles';
 import Conteiner from "./Conteiner";
 import LoginButtons from "./LoginButtons";
+import logo from "../../components/images/logo.png"
 
 
 const Login = () => {
   return (
     <div style={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
-      <h1 style={{textAlign: 'center', fontSize: '60px', marginTop: '10vh', marginBottom:'auto', fontFamily: 'Maven Pro',}}>LOGO</h1>
+      <div style={{marginTop: "8vh"}}> <img src={logo} alt="Logo Hackaton-Turismo" style={{height: "15vh", width: "15vh"}}/> </div>
       <Conteiner/>
       <LoginButtons/>
       <GlobalStyles/>
